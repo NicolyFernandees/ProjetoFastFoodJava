e4ebd215409849656fa478d510482a18baa60864
unknown <nycolysennaa@gmail.com>
2025-10-06 21:48:29 -0300

    Pedido, order, enums, realizado

---
+++ src/main/java/com/unicesumar/FastFood/LdaApplication.java
@@ -0,0 +1,13 @@
+package com.fastfood.lda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/unicesumar/FastFood/enums/PedidoStatus.java
@@ -0,0 +1,4 @@
+package com.fastfood.lda.enums;
+
+public class PedidoStatus {
+}
